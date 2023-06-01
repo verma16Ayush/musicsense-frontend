ebba257224101ad8aa1e068ac717e4464d99b9ee
Ayush <verma16.ayush@gmail.com>
2023-06-01 22:40:03 +0530

    cahnging the service to call to

--- src/getres.tsx
+++ src/getres.tsx
@@ -1,6 +1,5 @@
 export async function getResults(): Promise<IResType[]> {
-
-  return fetch("http://192.168.226.203:8000", {
+  return fetch("http://musicsense-backend:8000/", {
     method: "GET",
     redirect: "follow",
   }).then(res=>res.json()).catch((error) => console.log(error));
